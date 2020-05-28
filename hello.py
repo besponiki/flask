b6cddc8803731d914e1de75dbbe614d02e1c78eb
besponiki <dmitro.ozadovskiy@gmail.com>
2020-05-28 15:20:44 +0300

    fix broken email

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 
-    return 'Hello vasyadsfsdfdsfdsfsfd chlen! footer'
+    return 'Hello vasyadsfsdfdsfdsfdsjkfdskljsdfkjlsdfkjldsflkdsfsfd chlen! footer'
 
 if __name__ == '__main__':
     app.run()
