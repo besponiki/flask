e4d525eb666cf1bed707e5806bbdb13eeb226e74
besponiki <dmitro.ozadovskiy@gmail.com>
2020-05-28 15:32:44 +0300

    dsf

--- hello.py
+++ hello.py
@@ -3,12 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-
-<<<<<<< HEAD
     return 'Hello'
-=======
-    return 'Hello'
->>>>>>> task53
 
 if __name__ == '__main__':
     app.run()
