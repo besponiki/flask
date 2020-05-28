35e27476bddedd0c47960489d7b0410324bedb5b
besponiki <dmitro.ozadovskiy@gmail.com>
2020-05-28 15:25:15 +0300

    tiny changes

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
 
-    return 'Hello vasyadsfsdfdsfdsfsfd chlen! footer'
+    return 'Hello vasyadsfsdfdsfdsfsfd chlen! foote choto she delayur'
 
 if __name__ == '__main__':
     app.run()
